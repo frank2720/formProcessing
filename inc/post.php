39db9ebadfb163b080c5774c8fd77160e1dd57c5
frank2720 <otienof534@gmail.com>
2024-06-21 14:27:49 +0300

    filter_has_var usage

--- inc/post.php
+++ inc/post.php
@@ -1,6 +1,6 @@
 <?php
 
-//-----------------------------------------------------
+/*-----------------------------------------------------
 // WARNING: this doesn't include sanitization
 // and validation
 //-----------------------------------------------------
@@ -14,4 +14,11 @@ if (isset($_POST['name'], $_POST['email'])) {
 	echo "Please confirm it in your inbox of the email $email.";
 } else {
 	echo 'You need to provide your name and email address.';
+}*/
+
+if (filter_has_var(INPUT_POST,'name')&&filter_has_var(INPUT_POST,'email')) {
+    echo 'Thanks '.htmlspecialchars($_POST['name']) .' for your subscription.<br>';
+	echo "Please confirm it in your inbox of the email ".htmlspecialchars($_POST['email']);
+} else {
+    echo 'You need to provide your name and email address.';
 }
