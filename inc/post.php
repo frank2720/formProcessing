8e3ca429670e1dbab097300f061dd360ab57083b
frank2720 <otienof534@gmail.com>
2024-06-21 16:22:06 +0300

    form validation

--- inc/post.php
+++ inc/post.php
@@ -1,21 +1,47 @@
 <?php
 
-if (filter_has_var(INPUT_GET, 'id')) {
-    // sanitize id
-    $clean_id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter a valid email';
 
-    if ($clean_id) {
-        // validate id with options
-        $id = filter_var($clean_id, FILTER_VALIDATE_INT, ['options' => [
-            'min_range' => 10
-        ]]);
+// sanitize and validate name
+$name = filter_input(INPUT_POST, 'name');
+$inputs['name'] = $name;
 
-        // show the id if it's valid
-        echo $id === false ? 'id must be at least 10' : $id;
-    }
-    else {
-         echo 'id is invalid.';
+if ($name) {
+    $name = trim($name);
+    if ($name === '') {
+        $errors['name'] = NAME_REQUIRED;
     }
 } else {
-    echo 'id is required.';
+    $errors['name'] = NAME_REQUIRED;
 }
+
+
+// sanitize & validate email
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$inputs['email'] = $email;
+if ($email) {
+    // validate email
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    if ($email === false) {
+        $errors['email'] = EMAIL_INVALID;
+    }
+} else {
+    $errors['email'] = EMAIL_REQUIRED;
+}
+?>
+
+<?php if (count($errors) === 0) : ?>
+    <section>
+        <h2>
+            Thanks <?php echo htmlspecialchars($name) ?> for your subscription!
+        </h2>
+        <p>Please follow the steps below to complete your subscription:</p>
+        <ol>
+            <li>Check your email (<?php echo htmlspecialchars($email) ?>) - Find the message sent from webmaster@phptutorial.net</li>
+            <li>Click to confirm - Click on the link in the email to confirm your subscription.</li>
+        </ol>
+    </section>
+
+<?php endif ?>
